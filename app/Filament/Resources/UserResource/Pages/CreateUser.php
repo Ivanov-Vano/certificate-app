4f5559676c36536f977e7dbeed0038904dc72ba2
lin071 <linyushin85@gmail.com>
2023-11-06 11:43:07 +0300

    добавлен модуль администрирования

---
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Создан новый пользователь';
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+}
