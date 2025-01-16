1c889c08c1d7709bfcb70e908966b1378ec2e203
lin071 <linyushin85@gmail.com>
2025-01-17 00:57:37 +0300

    Изменена круговая диаграмма с учетом вывода первых 10

--- app/Filament/Resources/LeadResource/Widgets/LeadPieChart.php
+++ app/Filament/Resources/LeadResource/Widgets/LeadPieChart.php
@@ -7,11 +7,14 @@ use Filament\Widgets\ChartWidget;
 
 class LeadPieChart extends ChartWidget
 {
-    protected static ?string $heading = 'Круговая диаграмма количества заявок СПТ по лидам';
+    protected static ?string $heading = 'Рейтинг первых 10 Лидов по количеству заявок';
 
     protected function getData(): array
     {
-        $leads = Lead::withCount('applications')->get();
+        $leads = Lead::withCount('applications')
+            ->orderByDesc('applications_count')
+            ->take(10)
+            ->get();
 
         $labels = $leads->pluck('name')->toArray();
         $data = $leads->pluck('applications_count')->toArray();
